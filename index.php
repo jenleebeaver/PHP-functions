b0f1137efa68b9397287eb8a7d00d9a70b21f8c5
Jennifer Beaver <jen.lee.beaver@gmail.com>
2021-07-20 17:18:26 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// Write your code below:
+
+//Function Practice 
+
+//VOCAB:
+//function: keyword
+//name function: greetLearner
+//code block: anything in curly brackets {}
+//instructions: echo strings 
+//snakecase (snake_case) variables
+//camelCase functions 
+
+//now we can invoke this function multiple times 
+function greetLearner(){
+  echo "Hello, Learner!\n";
+  echo "I hope you're enjoying PHP!\n";
+  echo "Love, Codecademy\n";
+};
+
+greetLearner();
+
+function praisePHP(){
+  echo "I <3 PHP!";
+}
+
+praisePHP();
