19282bceefd4e25d6a54a87978db42f4a1d88655
Jennifer Beaver <jen.lee.beaver@gmail.com>
2021-07-21 14:57:56 -0700

    Create practice_returning_null.php

---
+++ practice_returning_null.php
@@ -0,0 +1,22 @@
+<?php
+//Example:
+function returnNothing() 
+{
+  echo "I'm running! I'm running!\n";
+}
+ 
+$result = returnNothing(); 
+// Prints: I'm running! I'm running!
+ 
+echo $result; 
+
+
+
+// Write your code below:
+  function createVacuum() {  
+  };
+
+  echo $createVacuum;
+  echo "\n";
+ 
+echo createVacuum() * 10;
